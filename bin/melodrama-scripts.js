bda2c5ea4102b2fd269a9e1b925a8871b8ccb6b2
Sebastian Sebald <sebastian.sebald@gmail.com>
2016-11-03 23:58:20 +0100

    :construction: call scripts from bin

---
+++ bin/melodrama-scripts.js
@@ -0,0 +1,57 @@
+#!/usr/bin/env node
+'use strict';
+
+const chalk = require('chalk');
+const spawn = require('child_process').spawn;
+const meow = require('meow');
+
+const cli = meow({
+  description: false,
+  help:
+`
+  ${chalk.underline('Usage:')}
+    $ melodrama-scripts <command> [options]
+
+  ${chalk.underline('Usage:')}
+    init [dir]        bootstrap project dir
+    start [file]      run dev server with file as entry
+
+  ${chalk.underline('Options:')}
+    -h, --help        output usage information
+    -v, --version     output the version number
+    --verbose         print logs while executing command
+`
+}, {
+  alias: {
+    v: 'version'
+  },
+  boolean: ['help', 'verbose', 'version']
+});
+
+
+/**
+ * Validate input.
+ */
+const script = cli.input.shift();
+if (!/init|start/.test(script)) {
+  console.log(chalk.bold(`Unknown script "${script}".`));
+  process.exit();
+}
+
+/**
+ * Parse input and execute script.
+ *
+ * NOTE: We can not use `execa` here, b/c it doesn't support
+ *       inheriting stdio, which is required to have `inquierer``
+ *       work in a child process :-(
+ */
+const ps = spawn(
+  'node',
+  [
+    require.resolve('../scripts/' + script),
+    cli.input.join(' ')
+  ],
+  { stdio: 'inherit' }
+);
+ps.on('err', err => console.log(chalk.red(err)));
+ps.on('close', code => console.log(code));
